d4f6e25014df8c8bce2ca993662aac5d9353c000
zcheng98 <zcheng6@nd.edu>
2022-07-12 13:18:45 -0400

    fixed the login bug
    Bug happened because of the update function with react

--- src/Components/Login/AuthLogin.js
+++ src/Components/Login/AuthLogin.js
@@ -8,60 +8,11 @@ import ProtectedRoute from '../../Common/AppTools/ProtectedRoute';
 import Header from '../Header/Header.js'
 
 function AuthLogin() {
-  const [newUser, setNewUser] = useState({
-    email: '',
-    password: '',
-  });
-
-  // flags in the state to watch for add/remove updates
-  const [flag, setFlag] = useState(false);
-  const navigate = useNavigate();
-
-  // useEffect that run when changes are made to the state variable flags
-  useEffect(() => {
-    if (newUser && flag) {
-      userLogin(newUser).then((loginResp) => {
-        if (loginResp) {
-          setFlag(true)
-          // TODO: redirect user to main app
-          navigate('/home?user='+newUser.email);
-          // navigate('/MainGood?user='+newUser.email);
-          
-          alert(
-            `${loginResp.get('email')}, login success!!`,
-          );
-        } else {
-          setFlag(false);
-        }
-      });
-    }
-  }, [newUser, flag]);
-
-  const onChangeHandler = (e) => {
-    e.preventDefault();
-    console.log(e.target);
-    const { name, value: newValue } = e.target;
-    console.log(newValue);
-
-    setNewUser({
-      ...newUser,
-      [name]: newValue,
-    });
-  };
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    console.log('submitted: ', e.target);
-    setFlag(true);
-  };
 
   return (
     <div>
       <Header />
       <ProtectedRoute component={() => <LoginForm
-        user={newUser}
-        onChange={onChangeHandler}
-        onSubmit={onSubmitHandler}
       />} currentUser={!Parse.User.current()} path='/home' />
       
     </div>
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,7 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {userLogin} from '../Auth/AuthService';
+import { Link, useNavigate } from 'react-router-dom';
 
-function LoginForm({ user, onChange, onSubmit }) {
+function LoginForm() {
+  const [newUser, setNewUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  // flags in the state to watch for add/remove updates
+  const [flag, setFlag] = useState(false);
+  const navigate = useNavigate();
+
+  // useEffect that run when changes are made to the state variable flags
+  useEffect(() => {
+    if (newUser && flag) {
+      userLogin(newUser).then((loginResp) => {
+        if (loginResp) {
+          setFlag(true)
+          // TODO: redirect user to main app
+          navigate('/home?user='+newUser.email);
+          // navigate('/MainGood?user='+newUser.email);
+          
+          alert(
+            `${loginResp.get('email')}, login success!!`,
+          );
+        } else {
+          setFlag(false);
+        }
+      });
+    }
+  }, [newUser, flag]);
+  
+  const onChange = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const { name, value: newValue } = e.target;
+    console.log(newValue);
+
+    setNewUser({
+      ...newUser,
+      [name]: newValue,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('submitted: ', e.target);
+    setFlag(true);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit} autoComplete="off">
@@ -12,7 +60,7 @@ function LoginForm({ user, onChange, onSubmit }) {
             type="email"
             className="form-control"
             id="email-input"
-            value={user.email}
+            value={newUser.email}
             onChange={onChange}
             name="email"
             required
@@ -26,7 +74,7 @@ function LoginForm({ user, onChange, onSubmit }) {
             type="password"
             className="form-control"
             id="password-input"
-            value={user.password}
+            value={newUser.password}
             onChange={onChange}
             name="password"
             min="0"
